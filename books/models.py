b0b82853e86ea3dc98b19bffab6a3c903acdcfc9
Xumimsl <xumimsl@gmail.com>
2025-11-08 17:32:10 +0600

    2

--- books/models.py
+++ books/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+
+
+
+    GENRE = (
+        ('Фантастика', 'Фантастика'),
+        ('Ужасы', 'Ужасы'),
+        ('Драма', 'Драма'),
+        ('Роман', 'Роман'),
+        ('Фэнтези', 'Фэнтези'),
+        ('История', 'История'),
+    )
+
+    title = models.CharField(max_length=100, verbose_name='Укажите название книги')
+    image = models.ImageField(upload_to='books/', verbose_name='Загрузите обложку')
+    description = models.TextField(verbose_name='Укажите аннотацию')
+    director = models.CharField(max_length=100, verbose_name='Укажите автора книги', default='неизвестен')
+    genre = models.CharField(max_length=100, choices=GENRE, verbose_name='Укажите жанр')
+    country = models.CharField(max_length=100, default='Кыргызстан', verbose_name='Укажите страну')
+    pages = models.PositiveIntegerField(verbose_name='Укажите количество страниц', default=0)
+    age = models.PositiveIntegerField(verbose_name='Укажите возрастное ограничение', default=8)
+    language = models.CharField(max_length=100, verbose_name='Укажите язык написания')
+    publishing_house = models.CharField(max_length=100, verbose_name='Укажите издательство')
+
+
+
+    def __str__(self):
+        return f"{self.title} ({self.director})"
--- books/urls.py
+++ books/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('about_me/', views.about_me, name='about_me'),
-    path('current_time/', views.current_time, name='current_time'),
-    path('random_quote/', views.random_quote, name='random_quote'),
+    # path('about_me/', views.about_me, name='about_me'),
+    # path('current_time/', views.current_time, name='current_time'),
+    # path('random_quote/', views.random_quote, name='random_quote'),
+    path('book_list/<int:id>/', views.bookDetailView, name='book_detail'),
+    path('book_list/', views.bookListView, name='book_list'),
     
 ]
